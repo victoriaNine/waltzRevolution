028c69196a34045a132cd884e995b6407383e19d
Victoria Nine <victoria@orion9.net>
2015-05-18 18:12:23 +0200

    added google analytics

--- app/js/main.js
+++ app/js/main.js
@@ -409,3 +409,15 @@ function tabletCheck() {
 	if(!phoneCheck() && mobileCheck()) check = true;
 	return check;
 }
+
+
+//===============================
+// GOOGLE ANALYTICS
+//===============================
+(function(b,o,i,l,e,r){
+	b.GoogleAnalyticsObject=l;b[l]||(b[l]=function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+	e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+	e.src='//www.google-analytics.com/analytics.js';
+	r.parentNode.insertBefore(e,r)
+}(window,document,'script','ga'));
+ga('create','UA-43190815-1');ga('send','pageview');
